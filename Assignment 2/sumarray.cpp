8296e56c2e6328a7d75380aadbd1ec1f777fadd6
Jeff Bohlin <jeffdbx@csu.fullerton.edu>
2015-02-08 18:26:22 -0800

    More small cleanup

--- Assignment 2/sumarray.cpp
+++ Assignment 2/sumarray.cpp
@@ -5,28 +5,28 @@
 // File name: sumarray.cpp
 // Function name: sumarray
 // Purpose: This module calculates the sum of all of the elements in an array.  It
-//	        then returns the result to 'vectordotproduct.asm'.
+//          then returns the result to 'vectordotproduct.asm'.
 //
 // Compile with this command: g++ -c -Wall -m64 -l sumarray.lis -o sumarray.o sumarray.cpp
 //
 #include <iostream>
 using namespace std;
 
-//Prototype:
+// Prototype:
 extern "C" long int sumarray(long int arr[], int n);
 
 
-//Definition:
+// Definition:
 // Note: Because I am using qwords in assembly, I MUST use long integers in arr[] here.  Before, I had
 //       only specified 'int arr[]' and each time I would have an error.
 
 long int sumarray(long int arr[], int counter)
 {
-	long int sum = 0;
+    long int sum = 0;
 
-	for (int i = 0; i < counter; i++)
-		sum += arr[i];
+    for (int i = 0; i < counter; i++)
+        sum += arr[i];
 
-    //return sum to rax
-	return sum;	
+    // return sum to rax
+    return sum;
 }
